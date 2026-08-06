d53d3e1096f577eb966260f01a46c8910635c0cc
Shreeja Singh <145353903+ShreejaSingh1@users.noreply.github.com>
2026-08-06 08:46:46 +0530

    Time: 0 ms (100%), Space: 43.2 MB (80.23%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry=1;
+        for(int i=digits.length-1;i>=0;i--){
+        if(digits[i]==9 && carry==1){
+            digits[i]=0;
+            carry=1;
+        }
+        else{
+            digits[i]+=carry;
+            carry=0;
+            return digits;
+
+        }
+        }
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+        
+    }
+}
